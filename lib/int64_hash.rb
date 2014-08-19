e5c5393a6992ff5864d38798cf2e321d473b73ef
Katie Hoffman <ktahoffman@gmail.com>
2014-08-19 17:39:07 -0400

    add two specs for #int64_hash

--- lib/int64_hash.rb
+++ lib/int64_hash.rb
@@ -3,14 +3,16 @@ def int64_hash(string)
   fin_num = 7
   i = 0
   while i < string.length
-    if valid_chars.include(string[i])
+    if valid_chars.include?(string[i])
       fin_num = 37 * fin_num + valid_chars.index(string[i])
     else
-      return "The word should only contain letters in '#{valid_chars}'"
+      return "The word should only contain letters in #{valid_chars}"
     end
     i += 1
   end
   fin_num
 end
 
-puts int64_hash("leepadg")
+def revsered_int64_hash(number)
+  # code goes here
+end
--- spec/int64_hash_spec.rb
+++ spec/int64_hash_spec.rb
@@ -1,2 +1,13 @@
-require_relative ".spec_helper.rb"
+require_relative "./spec_helper.rb"
 
+describe '#int64_hash' do 
+  it 'correctly calculates the final result' do
+    expect(int64_hash("leepadg")).to eq(680131659347)
+    expect(int64_hash("degilmnrs")).to eq(917052554341109)
+  end
+
+  it 'returns message about including proper characters' do
+    expect(int64_hash("degilmnrx")).to eq("The word should only contain letters in acdegilmnoprstuw")
+  end
+
+end
