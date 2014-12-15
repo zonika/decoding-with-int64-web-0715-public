c9df94a69ed96faaca3c49ba7ca40ad15eae5ab2
Katie Hoffman <ktahoffman@gmail.com>
2014-12-15 14:56:20 -0500

    add basic tests for 'a'

--- spec/encode_decode_spec.rb
+++ spec/encode_decode_spec.rb
@@ -2,12 +2,20 @@ require_relative "./spec_helper.rb"
 
 describe '#encode_int64' do 
 
-  it 'correctly calculates the final result' do
+  it "converts 'a' to 259" do
+    expect(encode_int64("a")).to eq(259)
+  end
+
+  it "converts 'leepadg' to 680131659347" do
     expect(encode_int64("leepadg")).to eq(680131659347)
+  end
+
+  it "converts 'degilmnrs' to 680131659347" do
     expect(encode_int64("degilmnrs")).to eq(917052554341109)
   end
 
-  it 'returns message about including proper characters' do
+  it 'returns message for strings that include invalid characters' do
+    expect(encode_int64("x")).to eq("The word should only contain letters in acdegilmnoprstuw")
     expect(encode_int64("degilmnrx")).to eq("The word should only contain letters in acdegilmnoprstuw")
   end
 
@@ -15,15 +23,25 @@ end
 
 describe '#decode_int64' do 
 
-  it 'correctly calculates the final result' do
+  it "converts 259 to 'a'" do
+    expect(decode_int64(259)).to eq("a")
+  end
+
+  it "converts 680131659347 to 'leepadg'" do
     expect(decode_int64(680131659347)).to eq("leepadg")
+  end
+
+  it "converts 917052554341109 to 'degilmnrs'" do
     expect(decode_int64(917052554341109)).to eq("degilmnrs")
   end
 
 end
 
-describe '#student_change' do
-  it 'find the characters that result in the value' do
-    expect(encode_int64("write answer here")).to eq(956446786872726)
+describe '#update_the_spec' do
+
+  it 'replace the question mark with a word that encodes as 956446786872726' do
+    answer = "?"
+    expect(encode_int64(answer)).to eq(956446786872726)
   end
+
 end
