d244c865249ec834d599a5b918279b7b3fe60d95
zonika <zchodosh@conncoll.edu>
2015-10-14 13:57:37 -0400

    done

--- lib/encode_decode.rb
+++ lib/encode_decode.rb
@@ -1,7 +1,27 @@
+require 'pry'
 def encode_int64(message)
-  # code this method first
+  letters = "acdegilmnoprstuw".split('')
+  h = 7
+  message.split('').each do |l|
+    if letters.index(l)
+      h = (h * 37 + letters.index(l))
+    else
+      h = "The word should only contain letters in acdegilmnoprstuw"
+      break
+    end
+  end
+  h
 end
 
 def decode_int64(encoded_message)
-  # code this second
+  e = encoded_message
+  letters = "acdegilmnoprstuw".split('')
+  ans = []
+  while e > 37 do
+    i = e%37
+    l = letters[i]
+    ans.push(l)
+    e = (e - i)/37
+  end
+  ans.join('').reverse
 end
